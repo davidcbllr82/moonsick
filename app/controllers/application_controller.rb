aab7a8d892d83b15cdd93e1abf9c1247cdd0cdb3
Katja KARPAC <katja.karpac@gmail.com>
2019-11-26 17:29:54 +0100

    add metadata to head

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  def default_url_options
+   { host: ENV["www.deyz.club"] || "localhost:3000" }
+  end
 end
