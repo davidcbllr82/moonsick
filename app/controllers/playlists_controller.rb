ebdfc494926fed18bcf50e6a354ac78e8f1206b2
Katja KARPAC <katja.karpac@gmail.com>
2019-11-26 16:31:06 +0100

    line deletion

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,4 +2,3 @@ class PlaylistsController < ApplicationController
 end
 
 
-<iframe src="https://open.spotify.com/embed/playlist/3U9lafWEnb3uP6JFbI65uq" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
