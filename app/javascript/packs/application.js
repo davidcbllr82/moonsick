16c063ff6a76f5b695670dca3588e366a513b765
Katja KARPAC <katja.karpac@gmail.com>
2019-11-28 10:58:04 +0100

    fix js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,21 @@ import "uikit";
 
 // behavior side navbar
 const toggling = document.getElementById('toggle');
-toggling.addEventListener('click', function() {
-  document.querySelector('.sidebar').classList.toggle('visible');
-});
+if (toggling) {
+  toggling.addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('visible');
+  });
+}
 
 // behavior gradient tracking button
 
 var btn = document.querySelector('.mouse-cursor-gradient-tracking')
-btn.onmousemove = function(e) {
-  var rect = e.target.getBoundingClientRect()
-  var x = e.clientX - rect.left
-  var y = e.clientY - rect.top
-  btn.style.setProperty('--x', x + 'px')
-  btn.style.setProperty('--y', y + 'px')
+if (btn) {
+  btn.onmousemove = function(e) {
+    var rect = e.target.getBoundingClientRect()
+    var x = e.clientX - rect.left
+    var y = e.clientY - rect.top
+    btn.style.setProperty('--x', x + 'px')
+    btn.style.setProperty('--y', y + 'px')
+  }
 }
