a7ee135ace6e8ab9775946303a48b226ac4e23b4
David Caballero <david.caballero@code.berlin>
2019-12-03 09:26:56 +0100

    profile information is now a card. drop shadow added to visors

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -2,7 +2,12 @@ module PlaylistsHelper
   def spotify_embed_player(spotify_playlist_id)
     url = "https://open.spotify.com/embed/playlist/#{spotify_playlist_id}"
 
-    tag.iframe src: url, width: 500, height: 420, frameborder: 0, allowtransparency: true, allow: "encrypted-media"
+    tag.iframe src: url,
+               width: 500,
+               height: 420,
+               frameborder: 0,
+               allowtransparency: true,
+               allow: "encrypted-media"
     # "<iframe src= width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>"
   end
 end
