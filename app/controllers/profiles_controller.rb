24cda3dfc33f79b80fc8378c0b35d63e615b80f2
Katja KARPAC <katja.karpac@gmail.com>
2019-12-04 11:06:59 +0100

    add event cards for profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,8 @@ class ProfilesController < ApplicationController
   def show
     @friendship = Friendship.new
     @friendships = @profile.friendships_as_receiver.count
+    # display a persons events
+    @events = @profile.events
   end
 
   def edit
