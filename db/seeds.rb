6da929ca6e9435c0096db1f1c4b0695f0d36995f
David Caballero <david.caballero@code.berlin>
2019-12-19 14:30:35 -0500

    Seeds changed and

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,28 +10,28 @@
 # generate 50 profiles
 
 
-# p "Creating 50 fake people"
+p "Creating 50 fake people"
 
-# 50.times do |u|
-#   url = "https://source.unsplash.com/random/?portrait"
-#   profile = User.new(
-#     email: Faker::Internet.unique.email,
-#     password: Faker::Alphanumeric.alphanumeric(number: 10),
-#     username: Faker::Name.unique.name,
-#     bio: Faker::Movies::VForVendetta.speech,
-#     location: Faker::Address.city,
-#     )
-#   profile.remote_profile_avatar_url = url
-#   profile.remote_profile_banner_url = "https://source.unsplash.com/random/?textures-patters"
-#   profile.spotify_top_1 = "spotify:track:1rgnBhdG2JDFTbYkYRZAku"
-#   profile.spotify_top_2 = "spotify:track:21jGcNKet2qwijlDFuPiPb"
-#   profile.spotify_top_3 = "spotify:track:7LzouaWGFCy4tkXDOOnEyM"
-#   profile.spotify_top_4 = "spotify:track:4tidSEPF7Dbxcdxv3VyIHd"
-#   profile.spotify_top_5 = "spotify:track:0Y2cjFzr3R7KLPeFyxaggg"
-#   profile.save
-# end
+50.times do |u|
+  url = "https://source.unsplash.com/random/?portrait"
+  profile = User.new(
+    email: Faker::Internet.unique.email,
+    password: Faker::Alphanumeric.alphanumeric(number: 10),
+    username: Faker::Name.unique.name,
+    bio: Faker::Movies::VForVendetta.speech,
+    location: Faker::Address.city,
+    )
+  profile.remote_profile_avatar_url = url
+  profile.remote_profile_banner_url = "https://source.unsplash.com/random/?textures-patters"
+  profile.spotify_top_1 = "spotify:track:1rgnBhdG2JDFTbYkYRZAku"
+  profile.spotify_top_2 = "spotify:track:21jGcNKet2qwijlDFuPiPb"
+  profile.spotify_top_3 = "spotify:track:7LzouaWGFCy4tkXDOOnEyM"
+  profile.spotify_top_4 = "spotify:track:4tidSEPF7Dbxcdxv3VyIHd"
+  profile.spotify_top_5 = "spotify:track:0Y2cjFzr3R7KLPeFyxaggg"
+  profile.save
+end
 
-# p "Here is 50 new Deyzers"
+p "Here is 50 new Deyzers"
 
 
 p "Creating 50 events"
@@ -66,7 +66,7 @@ users.each do |user|
   10.times do
     moment = Moment.new
     moment.user = user
-    moment.content = Faker::TvShows::RuPaul.quote
+    moment.content = Faker::TvShows::RickAndMorty.quote
     offset = rand(event_count)
     moment.event = Event.offset(offset).first
     moment.save
